e993a2dee61342e5286e40a189caf4a6463c3eb5
sflanagan33 <sflanagan33@gatech.edu>
2018-09-13 00:31:38 -0400

    World map

---
+++ Assets/WorldMap/Scripts/WorldMapCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EphemeralArena.WorldMap {
+	public class WorldMapCamera : MonoBehaviour {
+
+		[SerializeField] private Transform follow;
+		[SerializeField] private float followStrength;
+		[SerializeField] private Vector3 offset;
+
+		private void Start() {
+			transform.position = follow.position + offset;
+		}
+
+		private void Update() {
+			float dt = Time.deltaTime * 60f;
+			transform.position = Vector3.Lerp(transform.position, follow.position + offset, followStrength * dt);
+		}
+	}
+}
